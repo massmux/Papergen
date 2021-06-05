7aa78264607dfee021e6bc0471ea0a3e86dfea04
massmux <go@massmux.com>
2021-06-05 10:43:31 +0200

    formatting + bip39 key

--- entropy.py
+++ entropy.py
@@ -19,36 +19,32 @@
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 
 
-
-import subprocess,os,sys
 import hashlib
-import cv2,base64
-import sounddevice
 
+import base64
+import cv2
+import sounddevice
 
 """ system constants """
 
-NOISE_SAMPLE        = 30    # main sampling seconds
-SHA256_ROUNDS       = 2048  # sha256 rounds (number)
-NOISE_SAMPLE_SALT   = 5     # salt sampling seconds
-SAMPLE_RATE         = 44100 # samplerate
-SAMPLING_FMT        = 'wav'
-IMG_SAMPLES         = 64
-IMG_SAMPLES_SALT    = 8
-
+NOISE_SAMPLE = 30  # main sampling seconds
+SHA256_ROUNDS = 2048  # sha256 rounds (number)
+NOISE_SAMPLE_SALT = 5  # salt sampling seconds
+SAMPLE_RATE = 44100  # samplerate
+SAMPLING_FMT = 'wav'
+IMG_SAMPLES = 64
+IMG_SAMPLES_SALT = 8
 
 
 class entropy():
 
-    def __init__(self,source='mic'):
-        self.source=source
-        self.entropy=1
+    def __init__(self, source='mic'):
+        self.source = source
+        self.entropy = 1
         return
 
-
-    def _getsha256(self,z):
+    def _getsha256(self, z):
         return hashlib.sha256(z.encode('utf-8')).hexdigest()
-
 
     def _getMicSd(self):
         """
@@ -56,54 +52,52 @@ class entropy():
         """
         try:
             noise0 = sounddevice.rec(int(SAMPLE_RATE * NOISE_SAMPLE), samplerate=SAMPLE_RATE, channels=2, blocking=True)
-            salt0 = sounddevice.rec(int(SAMPLE_RATE * NOISE_SAMPLE_SALT), samplerate=SAMPLE_RATE, channels=2, blocking=True)
-            (noise,salt) =( hashlib.sha256(bytearray(b''.join(noise0))).hexdigest() , hashlib.sha256(bytearray(b''.join(salt0))).hexdigest() )
-            for i in range(0,SHA256_ROUNDS):
-                noise=self._getsha256(noise+salt)
+            salt0 = sounddevice.rec(int(SAMPLE_RATE * NOISE_SAMPLE_SALT), samplerate=SAMPLE_RATE, channels=2,
+                                    blocking=True)
+            (noise, salt) = (hashlib.sha256(bytearray(b''.join(noise0))).hexdigest(),
+                             hashlib.sha256(bytearray(b''.join(salt0))).hexdigest())
+            for i in range(0, SHA256_ROUNDS):
+                noise = self._getsha256(noise + salt)
         except:
-            noise=False
+            noise = False
         return noise
-
 
     def _getImgRnd(self):
         """ salt hashing and converting image data into 256 bits final hash """
-        img_rnd_result= self._getsha256( str(self.img_rnd['base'] ) ) 
-        salt = self._getsha256(str( self.img_rnd['salt'] ) )
-        for i in range(0,2048):
-            img_rnd_result=self._getsha256(img_rnd_result+salt)
+        img_rnd_result = self._getsha256(str(self.img_rnd['base']))
+        salt = self._getsha256(str(self.img_rnd['salt']))
+        for i in range(0, 2048):
+            img_rnd_result = self._getsha256(img_rnd_result + salt)
         return img_rnd_result
-
 
     def _takePhoto(self):
         """ taking multiple photos from webcam in order to create randomness. Returns data and salt. used device 0 """
         try:
             camera = cv2.VideoCapture(0)
-            (all_data,all_salt)=("","")
+            (all_data, all_salt) = ("", "")
             for i in range(IMG_SAMPLES):
                 return_value, image = camera.read()
                 ocurrent = base64.b64encode(image)
-                all_data=all_data+str(ocurrent)
+                all_data = all_data + str(ocurrent)
             for z in range(IMG_SAMPLES_SALT):
                 return_value, image = camera.read()
                 ocurrent = base64.b64encode(image)
-                all_salt=all_salt+str(ocurrent)
-            del(camera)
-            self.img_rnd={  'base': all_data,
+                all_salt = all_salt + str(ocurrent)
+            del (camera)
+            self.img_rnd = {'base': all_data,
                             'salt': all_salt
-                        }
+                            }
         except:
-            self.img_rnd=False
+            self.img_rnd = False
         return self.img_rnd
-
 
     def getEntropy(self):
         """ returns true entropy from chosen source """
-        if self.source=='mic':
-            self.entropy=self._getMicSd()
-        elif self.source=='photo':
+        if self.source == 'mic':
+            self.entropy = self._getMicSd()
+        elif self.source == 'photo':
             if self._takePhoto():
-                self.entropy=self._getImgRnd()
+                self.entropy = self._getImgRnd()
             else:
-                self.entropy=False
+                self.entropy = False
         return self.entropy
-    
--- keys.py
+++ keys.py
@@ -19,103 +19,104 @@
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 
 
+from binascii import hexlify
 
-import bit,bech32, binascii, hashlib
-from mnemonic import Mnemonic
-from binascii import hexlify, unhexlify
+import bech32
+import bit
+import hashlib
 import qrcode
+from mnemonic import Mnemonic
 
 """
 this class creates either standalone jbok one-address wallet or bip39 24-words mnemonic sequence,
 based on the entropy given as input.
 """
 
+
 class wallet():
 
-    def __init__(self,wType,wName='default',net='mainnet'):
-        self.type=wType
-        self.wallet_name=wName
-        self.network=net
-        self.wallet={}
+    def __init__(self, wType, wName='default', net='mainnet'):
+        self.type = wType
+        self.wallet_name = wName
+        self.network = net
+        self.wallet = {}
         return
 
-    def setNetwork(self,net='mainnet'):
-        self.network=net
+    def setNetwork(self, net='mainnet'):
+        self.network = net
         return net
 
-    def setEntropy(self,wEntropy):
-        self.entropy=wEntropy
+    def setEntropy(self, wEntropy):
+        self.entropy = wEntropy
         return wEntropy
 
-    def setWalletName(self,wName):
-        self.wallet_name=wName
+    def setWalletName(self, wName):
+        self.wallet_name = wName
         return wName
 
-    def _hash160(self,keyobj):
-        ripemd160=hashlib.new("ripemd160")
+    def _hash160(self, keyobj):
+        ripemd160 = hashlib.new("ripemd160")
         ripemd160.update(hashlib.sha256(keyobj.public_key).digest())
         return ripemd160.digest()
 
-    def _bech32enc(self,ohash160,network='mainnet'):
-        bechenc=bech32.encode("bc",0,ohash160) if network=='mainnet' else bech32.encode("tb",0,ohash160)
+    def _bech32enc(self, ohash160, network='mainnet'):
+        bechenc = bech32.encode("bc", 0, ohash160) if network == 'mainnet' else bech32.encode("tb", 0, ohash160)
         return bechenc
 
-    def _getsha256(self,z):
+    def _getsha256(self, z):
         return hashlib.sha256(z.encode('utf-8')).hexdigest()
 
     def qrGen(self):
         """ generate QR codes for WIF key and addresses """
         try:
-            (qr_wif,qr_addr,qr_segwit,qr_bech32) =(qrcode.make(self.wallet['WIF']),
-                                            qrcode.make(self.wallet['p2pkh'] ),
-                                            qrcode.make(self.wallet['p2wpkh-ps2h']),
-                                            qrcode.make(self.wallet['p2wpkh'])
-                                            )
-            qr_wif.save(self.wallet_name+"-WIF.png")
-            qr_addr.save(self.wallet_name+"-p2pkh.png")
-            qr_segwit.save(self.wallet_name+"-p2wpkh-p2sh.png")
-            qr_bech32.save(self.wallet_name+"-p2wpkh.png")
+            (qr_wif, qr_addr, qr_segwit, qr_bech32) = (qrcode.make(self.wallet['WIF']),
+                                                       qrcode.make(self.wallet['p2pkh']),
+                                                       qrcode.make(self.wallet['p2wpkh-ps2h']),
+                                                       qrcode.make(self.wallet['p2wpkh'])
+                                                       )
+            qr_wif.save(self.wallet_name + "-WIF.png")
+            qr_addr.save(self.wallet_name + "-p2pkh.png")
+            qr_segwit.save(self.wallet_name + "-p2wpkh-p2sh.png")
+            qr_bech32.save(self.wallet_name + "-p2wpkh.png")
             return True
         except:
             return False
 
-    
     def getJBOK(self):
-      """ creates a 1 key standalone JBOK wallet """
+        """ creates a 1 key standalone JBOK wallet """
 
-      """ define the key object """
-      key=bit.Key.from_hex(self.entropy) if self.network=='mainnet' else bit.PrivateKeyTestnet.from_hex(self.entropy)
+        """ define the key object """
+        key = bit.Key.from_hex(self.entropy) if self.network == 'mainnet' else bit.PrivateKeyTestnet.from_hex(
+            self.entropy)
 
-      """ private and public key values in hex format """
-      hex_k=key.to_hex()
-      hex_K=bit.utils.bytes_to_hex(key.public_key,True)
+        """ private and public key values in hex format """
+        hex_k = key.to_hex()
+        hex_K = bit.utils.bytes_to_hex(key.public_key, True)
 
-      """ calculate hash160 and bech32 address """
-      hex_hash160=self._hash160(key).hex()
-      bech32=self._bech32enc(self._hash160(key), self.network)
+        """ calculate hash160 and bech32 address """
+        hex_hash160 = self._hash160(key).hex()
+        bech32 = self._bech32enc(self._hash160(key), self.network)
 
-      wallet={  'name': self.wallet_name,
-            'network': 'bitcoin '+ self.network,
-            'private': hex_k,
-            'public': hex_K,
-            'hash160': hex_hash160,
-            'WIF': key.to_wif(),
-            'p2pkh': key.address,
-            'p2wpkh-ps2h': key.segwit_address,
-            'p2wpkh': bech32
-            }
-      self.wallet=wallet
-      return wallet
-
+        wallet = {'name': self.wallet_name,
+                  'network': 'bitcoin ' + self.network,
+                  'private': hex_k,
+                  'public': hex_K,
+                  'hash160': hex_hash160,
+                  'WIF': key.to_wif(),
+                  'p2pkh': key.address,
+                  'p2wpkh-ps2h': key.segwit_address,
+                  'p2wpkh': bech32
+                  }
+        self.wallet = wallet
+        return wallet
 
     def getBip39(self):
         """ creates a bip39 full entropic mnemonic as a HD wallet """
         mnemo = Mnemonic('english')
-        hash0=self._getsha256(self.entropy)
+        hash0 = self.entropy
         entropy_b = bytearray(hash0, 'utf-8')
-        entropy_hash =hashlib.sha256(entropy_b).digest()
+        entropy_hash = hashlib.sha256(entropy_b).digest()
         entropy = hexlify(entropy_hash)
         words = mnemo.to_mnemonic(entropy_hash)
-        self.words=words
+        self.words = words
         return words
-
